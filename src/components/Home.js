c4ab75ce8f797364c7ecae9cdc75eeff1c7a034b
Chris <eyolo87@gmail.com>
2022-09-01 19:44:09 -0500

    login not working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { Login, Register } from "./Login";
+//import login/signup, Routines and Activities
 
-const Home = () => {
+const Home = ({ APIURL }) => {
+	const [token, setToken] = useState("");
+	const [user, setUser] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState("");
 	// FOR UNREGISTERED VISITOR
 	// show signin/signup
 	// see tabbed navigation for Routines and Activities
@@ -9,6 +15,17 @@ const Home = () => {
 	//stay logged in between pages
 	// logout if logged in
 	// see tabbed navigations for Routines, My Routines, and Activities
+
+	return (
+		<div>
+			<header>
+				<h1>Hello World</h1>
+			</header>
+
+			{/* <Login setToken={setToken} APIURL={APIURL} /> */}
+			{/* <Register setToken={setToken} APIURL={APIURL} /> */}
+		</div>
+	);
 };
 
 export default Home;
