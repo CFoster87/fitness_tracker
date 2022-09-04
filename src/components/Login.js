53e5d5ba456906d5cec660d7507915646e882fd6
Noelke10 <Noelke10@gmail.com>
2022-09-04 11:52:11 -0500

    login deleted user object getting error

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,94 +9,92 @@ import { CssBaseline, Typography } from "@mui/material";
 const APIURL = "https://fitnesstrac-kr.herokuapp.com/api";
 
 function Login({ setToken, setIsLoggedIn }) {
-	// login
-	async function userLogin(username, password) {
-		return fetch(`${APIURL}/users/login`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				user: {
-					username: username,
-					password: password,
-				},
-			}),
-		})
-			.then((response) => response.json())
-			.then((result) => {
-				console.log(result);
-				return result.data.token;
-			})
-			.catch(console.error);
-	}
+  // login
+  async function userLogin(username, password) {
+    return fetch(`${APIURL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        return result.data.token;
+      })
+      .catch(console.error);
+  }
 
-	const history = useNavigate();
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const token = await userLogin(username, password);
-			if (token) {
-				sessionStorage.setItem("token", JSON.stringify(token));
-				setToken(token);
-				setIsLoggedIn(true);
-				history("/Homepage");
-				alert("You are logged in!");
-			} else {
-				alert("Username or passowrd are incorrect");
-			}
-		} catch (error) {
-			alert(error.message);
-		}
-	};
-	return (
-		<Container component='main' maxWidth='xs'>
-			<CssBaseline />
-			<Box
-				sx={{
-					marginTop: 15,
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
-				<Typography component='h1' variant='h4'>
-					Login
-				</Typography>
-				<Box component='form' onSubmit={handleSubmit}>
-					<TextField
-						margin='normal'
-						required
-						fullWidth
-						id='outlined'
-						label='Username'
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					></TextField>
-					<TextField
-						margin='normal'
-						required
-						fullWidth
-						id='outlined-required'
-						label='Password'
-						type='password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					></TextField>
-					<Button
-						type='submit'
-						fullWidth
-						variant='contained'
-						sx={{ mt: 3, mb: 2 }}
-					>
-						Login
-					</Button>
-				</Box>
-			</Box>
-		</Container>
-	);
+  const history = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = await userLogin(username, password);
+      if (token) {
+        sessionStorage.setItem("token", JSON.stringify(token));
+        setToken(token);
+        setIsLoggedIn(true);
+        history("/");
+        alert("You are logged in!");
+      } else {
+        alert("Username or passowrd are incorrect");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 15,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h4">
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="outlined"
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          ></TextField>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="outlined-required"
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></TextField>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default Login;
